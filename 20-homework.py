bccb376e093afd4702d3e707b8065d26066baf49
Ash <ramazan.erduran@hacettepe.edu.tr>
2022-07-07 16:26:19 +0300

    enter

--- 20-homework.py
+++ 20-homework.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 import numpy as np 
 from sklearn import datasets
+import warnings
+warnings.filterwarnings("ignore")
+
 
 iris = datasets.load_iris()
 X = iris.data[:,:]
@@ -50,3 +53,35 @@ from sklearn.neighbors import KNeighborsClassifier
 knn_class = KNeighborsClassifier(n_neighbors=3, metric="minkowski")
 knn_class.fit(X_train, y_train)
 knn_pred = knn_class.predict(X_valid)
+
+from sklearn.svm import SVC
+svc = SVC()
+svc.fit(X_train, y_train)
+svc_pred = svc.predict(X_valid)
+
+from sklearn.naive_bayes import GaussianNB
+gb = GaussianNB()
+gb.fit(X_train, y_train)
+gb_pred = gb.predict(X_valid)
+
+from sklearn.tree import DecisionTreeClassifier
+dc = DecisionTreeClassifier()
+dc.fit(X_train, y_train)
+dc_pred = dc.predict(X_valid)
+
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+rf_pred = rf.predict(X_valid)
+
+################
+# SUCCES RATES #
+print('_'*50)
+print('SUCCESS RATES:')
+print("Logistic: {0:.3f}%".format(log_class.score(X_valid,y_valid)*100))
+print("KNN: {0:.3f}%".format(knn_class.score(X_valid,y_valid)*100))
+print("SVC: {0:.3f}%".format(svc.score(X_valid,y_valid)*100))
+print("Naive Bayes: {0:.3f}%".format(gb.score(X_valid,y_valid)*100))
+print("Decision Tree: {0:.3f}%".format(dc.score(X_valid,y_valid)*100))
+print("Random Forest: {0:.3f}%".format(rf.score(X_valid,y_valid)*100))
+print('_'*50)
